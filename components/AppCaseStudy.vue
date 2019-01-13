40a21e645a842aa2fc389930844a647d3c9c34d1
jrah <jjames.home@gmail.com>
2019-01-13 04:41:16 +0000

    Add case study component

---
+++ components/AppCaseStudy.vue
@@ -0,0 +1,73 @@
+<template lang="html">
+  <section class="py-10">
+    <div class="text-center">
+      <h1 class="mb-12 font-bold text-4xl leading-tight">{{ componentTitle }}</h1>
+    </div>
+    <div
+      :class="'l:'+ componentFlexDirection"
+      class="container flex justify-between flex-col">
+      <div class="w-full l:w-1/2 flex items-center">
+        <div class="px-4">
+          <img
+            :src="componentImage"
+            :alt="componentSubHeading +' Image'">
+        </div>
+      </div>
+      <div class="p-10 w-full l:w-1/2 border-grey-lighter border-solid border-2 rounded-sm shadow">
+        <h2
+          v-if="componentSubHeading"
+          class="mt-0 f1 my-2 font-bold text-2xl leading-tight">{{ componentSubHeading }}</h2>
+        <p
+          v-if="componentParagraph"
+          class=" m-0 my-5 leading-normal">{{ componentParagraph }}</p>
+        <div class="flex justify-end">
+          <a
+            :href="componentHref"
+            class="bg-point hover:bg-point-light text-white font-bold py-2 px-4 border-b-4 border-point-dark hover:border-point rounded no-underline">Visit {{ componentTitle }}</a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+</template>
+
+<script>
+export default {
+  props: {
+    componentTitle: {
+      type: String,
+      required: true
+    },
+    componentSubHeading: {
+      type: String,
+      required: false
+    },
+    componentParagraph: {
+      type: String,
+      required: true
+    },
+    componentHref: {
+      type: String,
+      required: true,
+      default: "#"
+    },
+    componentImage: {
+      type: String,
+      required: true
+    },
+    componentBackgroundColor: {
+      type: String,
+      required: false,
+      default: ""
+    },
+    componentFlexDirection: {
+      type: String,
+      required: false,
+      default: "flex-row"
+    }
+  }
+};
+</script>
+
+<style lang="css">
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,99 +2,61 @@
   <div class="content">
     <app-header/>
 
-
-    <section class=" mb-5 py-10 text-center bg-grey-lightest">
-        <h1 class="mb-16 text-4xl">{{ home.offers.title }}</h1>
-        <div class="container m:flex l:flex justify-around">
-          <div
-            v-for="(block, index) in home.offers.block"
-            :key="index"
-            :name=" '' + index "
-            class="text-center ns:w-1/4">
-            <div class="w-32 m-auto">
-              <img
-                :src="block.icon"
-                alt="block.icon">
-            </div>
-            <h2 class="leading-normal text-lg mt-1 mb-1">{{ block.heading }}</h2>
-            <p class="leading-normal mt-0 mb-5">{{ block.paragraph }}</p>
-          </div>
-        </div>
-    </section>
-
-
-    <section class="py-10">
-      <div class="text-center">
-        <h1 class="mb-12 font-bold text-4xl leading-tight">{{ home.options.heading }}</h1>
-      </div>
-      <div class="container l:flex justify-between">
-        <div class="bg-no-repeat p-0 m-5 l:w-1/2">
+    <section class="py-10 text-center bg-grey-lightest">
+      <h1 class="mb-16 text-4xl">{{ home.offers.title }}</h1>
+      <div class="container flex flex-wrap justify-between">
+         <div
+          v-for="(block, index) in home.offers.block"
+          :key="index"
+          :name=" '' + index "
+          class="w-full m:w-1/2 l:w-1/4 px-4">
+          <div class="max-w-xs m-auto">
             <img
-              class="object-cover h-full w-full p-1"
-              :src="home.options.image.icon">
-        </div>
-        <div class=" text-right mb-5 bg-whiteback p-10 m-5  l:w-1/2 border-grey-lighter border-solid border-2 rounded-sm shadow">
-          <div class= "text-left">
-            <h2
-              v-if="home.options"
-              class="mt-0 f1 my-2 font-bold text-2xl leading-tight">{{ home.options.project.heading }}</h2>
-            <p
-              v-if="home.options"
-              class="m-0 my-5 leading-normal">{{ home.options.project.paragraph }}</p>
+              :src="block.icon"
+              :alt="block.icon">
           </div>
-          <a
-            :href="home.options.project.href"
-            class="mt-5 inline-flex no-underline bg-whiteback hover:bg-point text-blackbasic font-semibold hover:text-whiteback py-2 px-6 border rounded hover:border-transparent  border-blackbasic">{{ home.options.project.button }}</a>
+          <h2 class="leading-normal text-lg mt-1 mb-1">{{ block.heading }}</h2>
+          <p class="leading-normal mt-0 mb-5">{{ block.paragraph }}</p>
         </div>
       </div>
     </section>
+
 
 
     <section class="py-10 bg-grey-lightest">
-        <h1 class="text-center mb-16 text-4xl">{{ home.features.title }}</h1>
-        <div class="container  m:flex l:flex justify-around">
-          <div
-            v-for="(block, index) in home.features.block"
-            :key="index"
-            :name=" '' + index "
-            class="text-center ns:w-1/4">
-            <div class="w-48 m-auto">
-              <img
-                :src="block.icon"
-                alt="block.icon">
-            </div>
-            <h2 class="leading-normal text-lg mt-1 mb-1">{{ block.heading }}</h2>
-            <p class="leading-normal mt-0 ">{{ block.paragraph }}</p>
-          </div>
-        </div>
-    </section>
-
-    <section class="py-10">
-      <div class="text-center">
-        <h1 class="mb-12 font-bold text-4xl leading-tight">{{ home.options2.heading }}</h1>
-      </div>
-      <div class="container flex justify-between flex-col ns:flex-row">
-        <div class=" text-right mb-5 bg-whiteback p-10 m-5 m:1/4 l:w-1/2 border-grey-lighter border-solid border-2 rounded-sm shadow">
-          <div class= "text-left">
-            <h1
-              v-if="home.options2"
-              class="mt-0 f1 my-2 font-bold text-2xl leading-tight">{{ home.options2.project.heading }}</h1>
-            <p
-              v-if="home.options2"
-              class=" m-0 my-5 leading-normal">{{ home.options2.project.paragraph }}</p>
-          </div>
-          <a
-            :href="home.options2.project.href"
-            class="mt-5 inline-flex no-underline bg-whiteback hover:bg-point text-blackbasic font-semibold hover:text-whiteback py-2 px-6 border rounded hover:border-transparent  border-blackbasic">{{ home.options2.project.button }}</a>
-
-        </div>
-        <div class=" bg-whiteback bg-no-repeat p-0 m-5 m:3/4 l:w-1/2">
+      <h1 class="text-center mb-16 text-4xl">{{ home.features.title }}</h1>
+      <div class="container  m:flex l:flex justify-around">
+        <div
+          v-for="(block, index) in home.features.block"
+          :key="index"
+          :name=" '' + index "
+          class="text-center ns:w-1/4">
+          <div class="w-48 m-auto">
             <img
-              class="object-cover h-full w-full p-1"
-              :src="home.options2.image.icon">
+              :src="block.icon"
+              alt="block.icon">
+          </div>
+          <h2 class="leading-normal text-lg mt-1 mb-1">{{ block.heading }}</h2>
+          <p class="leading-normal mt-0 ">{{ block.paragraph }}</p>
         </div>
       </div>
     </section>
+
+
+    <app-case-study
+      :component-sub-heading="home.caseStudyTwo.subheading"
+      :component-href="home.caseStudyTwo.href"
+      :component-title="home.caseStudyTwo.heading"
+      :component-image="home.caseStudyTwo.image"
+      :component-paragraph="home.caseStudyTwo.paragraph"/>
+
+    <app-case-study
+      :component-flex-direction="'flex-row-reverse'"
+      :component-sub-heading="home.caseStudyOne.subheading"
+      :component-href="home.caseStudyOne.href"
+      :component-title="home.caseStudyOne.heading"
+      :component-image="home.caseStudyOne.image"
+      :component-paragraph="home.caseStudyOne.paragraph"/>
 
     <section class="py-10 bg-grey-lightest">
       <h1 class=" text-center text-4xl mb-12">{{ home.clients.title }}</h1>
@@ -103,9 +65,9 @@
           v-for="(org, index) in home.clients.organisation"
           :key="index"
           :name=" '' + index "
-          class="p-4 max-w-md w-48 "
           :src="org.image"
-          :alt="org.name">
+          :alt="org.name"
+          class="p-4 max-w-md w-48 ">
       </div>
     </section>
 
@@ -119,12 +81,12 @@
           <appForm/>
         </div>
         <div class="bg-whiteback p-10  m-5 mt-0 l:w-1/3 border-grey-lighter border-solid border-2 rounded-sm shadow">
-          <h2 class="leading-tight mt-2 mb-4 text-lg font-bold ">{{home.contactUs.text1.header}}</h2>
-          <p class="leading-normal ma-0 mb-4 f4 text-sm">{{home.contactUs.text1.paragraph}}</p>
-          <h2 class="leading-tight mt-2 mb-4 text-lg font-bold">{{home.contactUs.text2.header}}</h2>
-          <p class="leading-normal ma-0 f4 mb-4  text-sm">{{home.contactUs.text2.paragraph}}</p>
-          <h2 class="leading-tight mt-2 mb-4 text-lg font-bold">{{home.contactUs.text3.header}}</h2>
-          <p class="leading-normal ma-0 f4 text-sm">{{home.contactUs.text3.paragraph}}</p>
+          <h2 class="leading-tight mt-2 mb-4 text-lg font-bold ">{{ home.contactUs.text1.header }}</h2>
+          <p class="leading-normal ma-0 mb-4 f4 text-sm">{{ home.contactUs.text1.paragraph }}</p>
+          <h2 class="leading-tight mt-2 mb-4 text-lg font-bold">{{ home.contactUs.text2.header }}</h2>
+          <p class="leading-normal ma-0 f4 mb-4  text-sm">{{ home.contactUs.text2.paragraph }}</p>
+          <h2 class="leading-tight mt-2 mb-4 text-lg font-bold">{{ home.contactUs.text3.header }}</h2>
+          <p class="leading-normal ma-0 f4 text-sm">{{ home.contactUs.text3.paragraph }}</p>
         </div>
       </div>
     </section>
@@ -132,23 +94,29 @@
 </template>
 
 <script>
-import home from '~/content/home.json'
-import appHeader from '~/components/Header.vue'
-import splash from '~/components/Splash.vue'
-import appForm from '~/components/Form.vue'
+import home from "~/content/home.json";
+
+import appHeader from "~/components/Header.vue";
+import splash from "~/components/Splash.vue";
+import appForm from "~/components/Form.vue";
+import appCaseStudy from "~/components/AppCaseStudy.vue";
+import appFeatureBlock from "~/components/AppFeatureBlock.vue";
+
 
 export default {
   components: {
     appHeader,
     splash,
-    appForm
+    appForm,
+    appCaseStudy,
+    appFeatureBlock
   },
   data() {
     return {
       home
-    }
+    };
   }
-}
+};
 </script>
 
 <style scoped lang="scss">
