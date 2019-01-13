49ac1c1042a69af4bc14ed7a58fb33d63d7177d0
jrah <jjames.home@gmail.com>
2019-01-13 21:57:22 +0000

    Format nuxt config correctly

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,72 +1,68 @@
-const pkg = require('./package')
+const pkg = require("./package");
 
 module.exports = {
-  mode: 'universal',
+  mode: "universal",
 
   /*
   ** Headers of the page
   */
   head: {
-    title: 'WeCraft',
+    title: "WeCraft",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description },
-      { hid: 'og:title', name: 'og:title', content: 'WeCraft' },
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: pkg.description },
+      { hid: "og:title", name: "og:title", content: "WeCraft" },
       {
-        hid: 'og:description',
-        name: 'og:description',
-        content: 'Meta data content'
+        hid: "og:description",
+        name: "og:description",
+        content: "Meta data content"
       },
-      { hid: 'og:type', name: 'og:type', content: 'website' },
-      { hid: 'og:url', name: 'og:url', content: 'https://nuxtjs.org' },
-      { name: 'og:image', content: 'https://i.imgur.com/gRR2pPe.jpg' }
+      { hid: "og:type", name: "og:type", content: "website" },
+      { hid: "og:url", name: "og:url", content: "https://nuxtjs.org" },
+      { name: "og:image", content: "https://i.imgur.com/gRR2pPe.jpg" }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
       {
-        rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css?family=Major+Mono+Display'
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css?family=Major+Mono+Display"
       },
       {
-        rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css?familily=Noto+Sans+TC'
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css?familily=Noto+Sans+TC"
       },
       {
-        rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css?family=Open+Sans'
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css?family=Open+Sans"
       }
     ],
     script: [
-      { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }
+      { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" }
     ]
   },
 
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#fff' },
+  loading: { color: "#fff" },
 
   /*
   ** Global CSS
   */
-  css: [
-    '~/assets/css/tailwind.css'
-  ],
+  css: ["~/assets/css/tailwind.css"],
 
   /*
   ** Plugins to load before mounting the App
   */
-  plugins: [
-        { src: '~/plugins/vue-carousel.js', ssr: false }
-  ],
+  plugins: [{ src: "~/plugins/vue-carousel.js", ssr: false }],
 
   /*
   ** Nuxt.js modules
   */
   modules: [
     // Doc: https://github.com/nuxt-community/axios-module#usage
-    '@nuxtjs/axios'
+    "@nuxtjs/axios"
   ],
   /*
   ** Axios module configuration
@@ -86,4 +82,4 @@ module.exports = {
       // Run ESLint on save
     }
   }
-}
+};
