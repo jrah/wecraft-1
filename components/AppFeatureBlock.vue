bb3339ab1c59f591cbebb67120f74630bc4b7779
jrah <jjames.home@gmail.com>
2019-01-14 00:01:43 +0000

    Prettify components

--- components/AppFeatureBlock.vue
+++ components/AppFeatureBlock.vue
@@ -1,22 +1,24 @@
 <template lang="html">
 
-  <section :class="componentBackgroundColor" class="bg-grey-lightest">
+  <section 
+    :class="componentBackgroundColor" 
+    class="bg-grey-lightest">
     <div class="container py-10">
       <h1 class="mb-16 text-4xl text-center">{{ componentTitle }}</h1>
       <div class="flex flex-wrap justify-between">
         <div
-         v-for="(block, index) in componentLoop"
-         :key="index"
-         :name=" '' + index "
-         class="w-full m:w-1/2 l:w-1/4 px-4">
-         <div class="max-w-xs m-auto">
-           <img
-             :src="block.icon"
-             :alt="block.icon">
-         </div>
-         <h2 class="leading-normal text-lg mt-1 mb-1">{{ block.heading }}</h2>
-         <p class="leading-normal mt-0 mb-5">{{ block.paragraph }}</p>
-       </div>
+          v-for="(block, index) in componentLoop"
+          :key="index"
+          :name=" '' + index "
+          class="w-full m:w-1/2 l:w-1/4 px-4">
+          <div class="max-w-xs m-auto">
+            <img
+              :src="block.icon"
+              :alt="block.icon">
+          </div>
+          <h2 class="leading-normal text-lg mt-1 mb-1">{{ block.heading }}</h2>
+          <p class="leading-normal mt-0 mb-5">{{ block.paragraph }}</p>
+        </div>
       </div>
     </div>
   </section>
@@ -33,7 +35,7 @@ export default {
     componentBackgroundColor: {
       type: String,
       required: false,
-      default: ''
+      default: ""
     },
     componentLoop: {
       type: Array,
--- components/Header.vue
+++ components/Header.vue
@@ -1,25 +1,25 @@
 <template>
-    <div
-      :style="{'background-image' : 'url('+home.image+')'}"
-      class="bg-cover bg-center ns:h-screen relative bg-grey-darkest">
-      <div class="container h-full relative ns:inline-flex flex-col justify-center pt-24 pb-12 ns:h-100 z-10">
-        <div class="m:w-3/5 l:w-2/5">
-          <h1>
-            <span class="text-point font-majorMonoDisplay mt-0 f1 mb-2 text-5xl l:text-headline leading-tight">{{ home.title }}</span>
-            <span
-              v-if="home.subtitle"
-              class="text-whiteback mt-0 f1 mb-2 text-3xl leading-normal">{{ home.subtitle }}</span>
-          </h1>
-          <p
-            v-if="home.intro"
-            class="text-whiteback m-0 mb-12 leading-normal">{{ home.intro }}</p>
-          <a
-            :href="home.contactEmail"
-            class="no-underline inline bg-transparent hover:bg-point text-whiteback font-bold hover:text-whiteback py-2 px-6 border-2 border-whiteback hover:border-transparent">{{ home.button }}</a>
-        </div>
+  <div
+    :style="{'background-image' : 'url('+home.image+')'}"
+    class="bg-cover bg-center ns:h-screen relative bg-grey-darkest">
+    <div class="container h-full relative ns:inline-flex flex-col justify-center pt-24 pb-12 ns:h-100 z-10">
+      <div class="m:w-3/5 l:w-2/5">
+        <h1>
+          <span class="text-point font-majorMonoDisplay mt-0 f1 mb-2 text-5xl l:text-headline leading-tight">{{ home.title }}</span>
+          <span
+            v-if="home.subtitle"
+            class="text-whiteback mt-0 f1 mb-2 text-3xl leading-normal">{{ home.subtitle }}</span>
+        </h1>
+        <p
+          v-if="home.intro"
+          class="text-whiteback m-0 mb-12 leading-normal">{{ home.intro }}</p>
+        <a
+          :href="home.contactEmail"
+          class="no-underline inline bg-transparent hover:bg-point text-whiteback font-bold hover:text-whiteback py-2 px-6 border-2 border-whiteback hover:border-transparent">{{ home.button }}</a>
       </div>
-      <div class="overlay"></div>
     </div>
+    <div class="overlay"/>
+  </div>
 
 </template>
 
