1a7c89445c9c6a126b822bd8974e53a24293fada
jrah <jjames.home@gmail.com>
2019-01-14 02:04:59 +0000

    Reorder content

--- pages/index.vue
+++ pages/index.vue
@@ -3,12 +3,12 @@
     <app-header/>
     <app-feature-block
       :component-background-color="'bg-grey-lightest'"
-      :component-title="home.features.title"
-      :component-loop="home.features.block"/>
-    <app-feature-block
-      :component-background-color="'bg-grey-lightest'"
       :component-title="home.services.title"
       :component-loop="home.services.block"/>
+    <app-feature-block
+      :component-background-color="'bg-grey-lightest'"
+      :component-title="home.features.title"
+      :component-loop="home.features.block"/>
     <app-case-study
       :component-sub-heading="home.caseStudyTwo.subheading"
       :component-href="home.caseStudyTwo.href"
