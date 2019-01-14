74d9f9dab548e4e83f619c0dc65d4a6838a5cf7f
jrah <jjames.home@gmail.com>
2019-01-14 00:35:29 +0000

    Soften blackbasic

--- tailwind.js
+++ tailwind.js
@@ -46,7 +46,7 @@ let colors = {
   transparent: "transparent",
 
   black: "#22292f",
-  blackbasic: "#241C15",
+  blackbasic: "#394053",
   "grey-darkest": "#3d4852",
   "grey-darker": "#606f7b",
   "grey-dark": "#8795a1",
