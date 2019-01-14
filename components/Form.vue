059eded55fb8aeda977f82f432be9241a00777d5
jrah <jjames.home@gmail.com>
2019-01-14 05:41:51 +0000

    Revert "Make same as tea dictionary"
    This reverts commit fc489fe80cf7edd60e423caff6062004d52881a3.

--- components/Form.vue
+++ components/Form.vue
@@ -4,6 +4,7 @@
     action=""
     name="contact"
     method="post"
+    @submit.prevent="validateBeforeSubmit"
     netlify-honeypot="bot-field">
     <input
       type="hidden"
@@ -37,9 +38,9 @@
     </div>
 
     <div class="flex justify-end">
-      <input
+      <button
         class="inline-flex cursor-pointer bg-transparent hover:bg-point text-point-dark font-semibold hover:text-white py-2 px-4 border border-point hover:border-transparent rounded"
-        type="submit" value="Submit" @click="validateBeforeSubmit"/>
+        type="submit">Submit</button>
     </div>
 
   </form>
