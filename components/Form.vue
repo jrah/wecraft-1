5eb00b851f16f7cc32f4b9de407e74e5639a9b18
jrah <jjames.home@gmail.com>
2019-01-17 22:18:49 +0000

    Add name

--- components/Form.vue
+++ components/Form.vue
@@ -14,37 +14,37 @@
     </p>
 
     <div class="column is-12">
-      <label 
-        class="block text-blackbasic font-bold md:text-right mb-1 md:mb-0 pr-4" 
+      <label
+        class="block text-blackbasic font-bold md:text-right mb-1 md:mb-0 pr-4"
         for="email">Name</label>
-      <input 
-        class="appearance-none block w-full bg-grey-lightest text-grey-darker border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" 
-        name="name" 
-        type="text" 
-        placeholder="Email">
+      <input
+        class="appearance-none block w-full bg-grey-lightest text-grey-darker border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+        name="name"
+        type="text"
+        placeholder="Name">
     </div>
 
     <div class="column is-12">
-      <label 
-        class="block text-blackbasic font-bold md:text-right mb-1 md:mb-0 pr-4" 
+      <label
+        class="block text-blackbasic font-bold md:text-right mb-1 md:mb-0 pr-4"
         for="email">Email</label>
-      <input 
-        class="appearance-none block w-full bg-grey-lightest text-grey-darker border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" 
-        name="email" 
-        type="text" 
+      <input
+        class="appearance-none block w-full bg-grey-lightest text-grey-darker border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+        name="email"
+        type="text"
         placeholder="Email">
     </div>
 
 
 
     <div class="column is-12">
-      <label 
-        class="block text-blackbasic font-bold md:text-right mb-1 md:mb-0 pr-4" 
+      <label
+        class="block text-blackbasic font-bold md:text-right mb-1 md:mb-0 pr-4"
         for="message">Message</label>
-      <textarea 
-        name="message" 
-        class="w-full h-32 bg-grey-lightest text-grey-darker border rounded py-3 px-4 mb-3 leading-tight focus:outline-none" 
-        style="vertical-align: top;" 
+      <textarea
+        name="message"
+        class="w-full h-32 bg-grey-lightest text-grey-darker border rounded py-3 px-4 mb-3 leading-tight focus:outline-none"
+        style="vertical-align: top;"
         type="textarea"/>
     </div>
 
